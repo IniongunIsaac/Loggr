e569c5bf121ed68afdd147848a5bd10563473355
Isaac Iniongun <iniongunisaac@gmail.com>
2023-10-25 11:57:51 +0100

    create logger

--- Sources/Loggr/Loggr.swift
+++ Sources/Loggr/Loggr.swift
@@ -1,2 +1,33 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+import os.log
+
+public final class Loggr {
+            
+    public static func logDebug(_ msg: String) {
+        #if DEBUG
+        os_log(.debug, "[DEBUG]: %s", msg)
+        #endif
+    }
+    
+    public static func logInfo(_ msg: String) {
+        #if DEBUG
+        os_log(.debug, "[INFO]: %s", msg)
+        #endif
+    }
+    
+    public static func logWarning(_ msg: String) {
+        #if DEBUG
+        os_log(.fault, "[WARNING]: %s", msg)
+        #endif
+    }
+    
+    public static func logError(_ msg: String) {
+        #if DEBUG
+        os_log(.error, "[ERROR]: %s", msg)
+        #endif
+    }
+
+}
